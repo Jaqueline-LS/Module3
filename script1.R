391a3670c5cae5ad103caccecae9f4281e074f56
Jaqueline-LS <jaquelinelamas@ice.ufjf.br>
2022-10-30 11:05:04 -0300

    Foi criado um template,automatizado com o parametro da regiao, onde foram gerados 9 documentos, um para cada região

--- script1.R
+++ script1.R
@@ -11,3 +11,19 @@ ggplot(sdat,
        aes(x = steak_prep, fill = female)) +
   geom_bar(position="dodge") +
   ggtitle(paste0("Steak Preparation Preference by Gender for ","Mountain"," Region"))
+
+
+rmarkdown::render("C:/Reproducible-Templates/Module3/steakArticleParameters.Rmd",
+                  params = list(region = "South Atlantic"))
+
+render_report <-function(regionvar){
+  template <-"C:/Reproducible-Templates/Module3/steakArticleParameters.Rmd"
+  outfile <-sprintf("steakArticle_%s.html",regionvar)
+  parameters <-list(region = regionvar)
+  rmarkdown::render(template,
+                    output_file=outfile,
+                    params=parameters)
+  invisible(TRUE)
+}
+
+render_report("Pacific")
